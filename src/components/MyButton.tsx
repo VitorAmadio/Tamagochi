012611d92eebdc79f29b044bee3df52bd44295e5
Vitor Godoy Amadio <132696370+VitorAmadio@users.noreply.github.com>
2023-09-05 19:38:08 -0300

    Atualização tela de login

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -4,21 +4,35 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 const styles = StyleSheet.create({
   buttonContainer: {
     padding: 16,
+    alignItems:"center"
+    
   },
   title: {
     fontSize: 20,
+    color: '#000'
   },
 });
 
 type ButtonProps ={
     title : string
     onPressButton: ()=>void
-    containerStyle?:
+    containerStyle:{
+      width: number;
+      height: number;
+      backgroundColor: string;
+      borderRadius: number;
+      margin: number;
+
+    }
 }
-const MyButton = ({title,onPressButton, color}:ButtonProps) => {
+const MyButton = ({title,onPressButton, containerStyle}:ButtonProps) => {
+  console.log({title})
     return (
+      
       <TouchableOpacity onPress={onPressButton}>
-      <View style={{...styles.buttonContainer, backgroundColor: color}}>
+      <View style={{...styles.buttonContainer, width:containerStyle.width,
+         height:containerStyle.height, backgroundColor:containerStyle.backgroundColor, borderRadius:containerStyle.borderRadius, margin:containerStyle.margin}}>
+          
         <Text style={styles.title}>{title}</Text>
       </View>
     </TouchableOpacity>
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Checkbox from 'expo-checkbox';
-import { TextInput, SafeAreaView, StyleSheet } from "react-native";
+import { TextInput, SafeAreaView, StyleSheet, View, Text, Alert } from "react-native";
 import MyButton from "../components/MyButton";
 
 
@@ -18,6 +18,10 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         margin: 10,
     },
+    senha:{
+        flexDirection: "row",
+        
+    }
 });
 
 const Login = () => {
@@ -25,25 +29,39 @@ const Login = () => {
     const [password, setPassword] = useState<string>();
     const [useSecurity, setUseSecurity] = useState<boolean>(true);
 
-    const alterScreen = ()=>{
-
+    const styleButton = {
+        width:150,
+        backgroundColor:'#274383',
+        height:60,
+        borderRadius:10,
+        margin:20
     }
+
+    const validateLogin = ()=>{
+        if (!login || !password){
+            Alert.alert('Erro', 'Informe o Login e a Senha para se conectar', [
+                {text: 'OK', onPress: () => console.log('Ok')},
+              ]);
+        }
+    }
+    const alterScreen = ()=>{
+        validateLogin()
+    }
+
 
     return (
         <SafeAreaView style={styles.container}>
-            <TextInput value={login} style={styles.input} placeholder="Username" />
-            <TextInput value={password} secureTextEntry={useSecurity} style={styles.input} placeholder="Password" />
-            <Checkbox
-            value={useSecurity}
-            onValueChange={setUseSecurity}
-            />
-            <MyButton title="Login" onPressButton={alterScreen} color="#f55" />
+            <TextInput value={login} style={styles.input} placeholder="Username" onChangeText={setLogin} />
+            <TextInput value={password} secureTextEntry={useSecurity} style={styles.input} placeholder="Password" onChangeText={setPassword} />
+            <View style={styles.senha}>
+                <Text>Esconder Senha  </Text>
+                <Checkbox value={useSecurity} onValueChange={setUseSecurity}/>
+            </View>
+            
+            <MyButton title="Login" onPressButton={alterScreen} containerStyle={styleButton} />
 
         </SafeAreaView>
     );
 };
 
 export default Login;
-//<TouchableOpacity onPress={togglePasswordVisibility}>
- //               <EyeIcon name={useSecurity ? "visibility-off" : "visibility"} size={50} /> 
-  //          </TouchableOpacity>
