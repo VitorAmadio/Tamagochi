691645b68fd882219755d350429a9a1a825ef664
Vitor Godoy Amadio <132696370+VitorAmadio@users.noreply.github.com>
2023-09-18 20:39:08 -0300

    Tela de login com utilização de API e cadastro de usuário

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -2,73 +2,81 @@ import React, { useState } from "react";
 import Checkbox from 'expo-checkbox';
 import { TextInput, SafeAreaView, StyleSheet, View, Text, Alert, Image } from "react-native";
 import MyButton from "../components/MyButton";
-
+import axios from "axios";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#4968B7 '
-    },
-    input: {
-        height: 50,
-        width: 250,
-        borderRadius: 3,
-        borderColor: 'black',
-        borderWidth: 2,
-        margin: 10,
-    },
-    senha:{
-        flexDirection: "row",
-        
-    },
-    image:{
-        width:400,
-        height:100
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4968B7'
+  },
+  input: {
+    height: 50,
+    width: 250,
+    borderRadius: 3,
+    borderColor: 'black',
+    borderWidth: 2,
+    margin: 10,
+  },
+  senha: {
+    flexDirection: "row",
+  },
+  image: {
+    width: 400,
+    height: 100
+  }
 });
 
 const Login = () => {
-    const [login, setLogin] = useState<string>();
-    const [password, setPassword] = useState<string>();
-    const [useSecurity, setUseSecurity] = useState<boolean>(true);
+  const [login, setLogin] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [useSecurity, setUseSecurity] = useState<boolean>(true);
 
-    const styleButton = {
-        width:150,
-        backgroundColor:'#274383',
-        height:60,
-        borderRadius:10,
-        margin:20,
-        color: '#fff'
+  const styleButton = {
+    width: 150,
+    backgroundColor: '#274383',
+    height: 60,
+    borderRadius: 10,
+    margin: 20,
+    color: '#fff'
+  }
+
+  const validateLogin = () => {
+    if (!login || !password) {
+      Alert.alert('Erro', 'Informe o Login e a Senha para se conectar', [
+        { text: 'OK', onPress: () => console.log('Ok') },
+      ]);
+    } else {
+      alterScreen();
     }
+  }
 
-    const validateLogin = ()=>{
-        if (!login || !password){
-            Alert.alert('Erro', 'Informe o Login e a Senha para se conectar', [
-                {text: 'OK', onPress: () => console.log('Ok')},
-              ]);
-        }
+  const alterScreen = async () => {
+    try {
+      const response = await axios.post('https://tamagochiapi-clpsampedro.b4a.run/login', {
+        email: login,
+        password: password,
+      });
+      console.log('Resposta do servidor: aaa');
+    } catch (error) {
+      console.log('Erro ao fazer login:', error);
     }
-    const alterScreen = ()=>{
-        validateLogin()
-    }
+  };
 
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={require('../images/Tamagotchi.png')} style={styles.image} />
+      <TextInput value={login} style={styles.input} placeholder="Usuário" onChangeText={setLogin} />
+      <TextInput value={password} secureTextEntry={useSecurity} style={styles.input} placeholder="Senha" onChangeText={setPassword} />
+      <View style={styles.senha}>
+        <Text>Esconder Senha  </Text>
+        <Checkbox value={useSecurity} onValueChange={setUseSecurity} />
+      </View>
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <Image source={require('../images/Tamagotchi.png')} style={styles.image}/>
-            <TextInput value={login} style={styles.input} placeholder="Usuário" onChangeText={setLogin} />
-            <TextInput value={password} secureTextEntry={useSecurity} style={styles.input} placeholder="Senha" onChangeText={setPassword} />
-            <View style={styles.senha}>
-                <Text>Esconder Senha  </Text>
-                <Checkbox value={useSecurity} onValueChange={setUseSecurity}/>
-            </View>
-            
-            <MyButton title="Login" onPressButton={alterScreen} containerStyle={styleButton} />
-
-        </SafeAreaView>
-    );
+      <MyButton title="Login" onPressButton={validateLogin} containerStyle={styleButton} />
+    </SafeAreaView>
+  );
 };
 
 export default Login;
